cf403b535149e2d228fdeb927ef2d93213f1559b
Cratairx <126484916+Cratairx@users.noreply.github.com>
2026-05-13 19:06:52 +0200

    commit 1

--- src/main/java/org/example/assignment_backend_one/AssignmentBackendOneApplication.java
+++ src/main/java/org/example/assignment_backend_one/AssignmentBackendOneApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AssignmentBackendOneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AssignmentBackendOneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentBackendOneApplication.class, args);
+    }
 
 }
