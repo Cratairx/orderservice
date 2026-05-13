6b65c75d097564871b7379ffcb71c5a973ef3a0f
Cratairx <126484916+Cratairx@users.noreply.github.com>
2026-05-13 19:05:56 +0200

    Initial commit

---
+++ src/main/java/org/example/assignment_backend_one/AssignmentBackendOneApplication.java
@@ -0,0 +1,13 @@
+package org.example.assignment_backend_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentBackendOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentBackendOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/assignment_backend_one/AssignmentBackendOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.assignment_backend_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentBackendOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
