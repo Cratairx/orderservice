cd87997958201371a9d4a27fef34a34393286485
Cratairx <126484916+Cratairx@users.noreply.github.com>
2026-05-14 13:10:26 +0200

    håller på med editcustomer.html

--- src/main/java/org/example/assignment_backend_one/Controllers/CustomerController.java
+++ src/main/java/org/example/assignment_backend_one/Controllers/CustomerController.java
@@ -1,7 +1,32 @@
 package org.example.assignment_backend_one.Controllers;
 
+import jakarta.servlet.http.HttpSession;
+import org.example.assignment_backend_one.Repositories.CustomerRepository;
+import org.example.assignment_backend_one.Services.CustomerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController( CustomerService customerService) {
+                this.customerService = customerService;
+    }
+
+    @GetMapping("/editcustomer")
+    public String editCustomer(@RequestParam Long id, Model model, HttpSession session) {
+        String firstName = (String) session.getAttribute("firstName");
+        String lastName = (String) session.getAttribute("lastName");
+        String email = (String) session.getAttribute("email");
+        model.addAttribute("customer", customerService.getCustomerById(id));
+        model.addAttribute("firstName", firstName);
+        model.addAttribute("lastName", lastName);
+        model.addAttribute("email", email);
+
+        return "editcustomer";
+    }
 }
--- src/main/java/org/example/assignment_backend_one/Services/CustomerService.java
+++ src/main/java/org/example/assignment_backend_one/Services/CustomerService.java
@@ -1,7 +1,21 @@
 package org.example.assignment_backend_one.Services;
 
+import org.example.assignment_backend_one.Models.Customer;
+import org.example.assignment_backend_one.Repositories.CustomerRepository;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+
+    CustomerRepository customerRepository;
+
+  public CustomerService(CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
+
+  }
+
+  public Customer getCustomerById(Long id) {
+    return customerRepository.findById(id).orElse(null);
+  }
 }
