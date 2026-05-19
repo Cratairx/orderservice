374288bf7bd7cac72052117f41ea8511023fbd64
Cratairx <daniel.inserte@gmail.com>
2026-05-19 13:32:25 +0200

    Fixat lite med regga kund. Ej Klar

--- src/main/java/org/example/assignment_backend_one/Services/implents/RoomImpl.java
+++ src/main/java/org/example/assignment_backend_one/Services/implents/RoomImpl.java
@@ -1,6 +1,16 @@
 package org.example.assignment_backend_one.Services.implents;
 
-public interface RoomImpl {
+import org.example.assignment_backend_one.Repositories.RoomRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+  public class RoomImpl {
     // interface till DTOER
+
+    private RoomRepository roomRepository;
+    public RoomImpl(RoomRepository roomRepository) {
+    this.roomRepository = roomRepository;
+}
+
 
 }
