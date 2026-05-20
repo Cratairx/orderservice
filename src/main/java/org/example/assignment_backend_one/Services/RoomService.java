c16b46d46653cded6e647d0ae538ea858c58b79d
Cratairx <126484916+Cratairx@users.noreply.github.com>
2026-05-20 20:03:09 +0200

    wTF buggar

--- src/main/java/org/example/assignment_backend_one/Services/RoomService.java
+++ src/main/java/org/example/assignment_backend_one/Services/RoomService.java
@@ -1,14 +1,4 @@
 package org.example.assignment_backend_one.Services;
 
-import org.example.assignment_backend_one.Models.Room;
-
-
-public interface RoomService {
-
-
-    public void getAllRooms(Room  room);
-
-
-
-
+public class RoomService {
 }
