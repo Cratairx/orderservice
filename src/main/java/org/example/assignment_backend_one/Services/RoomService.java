6523737e528314fc306379ea3c65a187a13ca41c
Cratairx <126484916+Cratairx@users.noreply.github.com>
2026-05-20 20:14:29 +0200

    wTF buggar

--- src/main/java/org/example/assignment_backend_one/Services/RoomService.java
+++ src/main/java/org/example/assignment_backend_one/Services/RoomService.java
@@ -1,4 +1,25 @@
 package org.example.assignment_backend_one.Services;
 
+
+import org.example.assignment_backend_one.Models.Room;
+import org.example.assignment_backend_one.Repositories.RoomRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class RoomService {
+
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+
+    public Room saveRoom(Room room) {
+        return roomRepository.save(room);
+    }
 }
