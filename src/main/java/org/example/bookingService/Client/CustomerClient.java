ca967b6c08702ebeb781070ddf512013dcb27907
Cratairx <daniel.inserte@gmail.com>
2026-09-22 15:45:27 +0200

    testing on laptop that everything works

--- src/main/java/org/example/bookingService/Client/CustomerClient.java
+++ src/main/java/org/example/bookingService/Client/CustomerClient.java
@@ -14,7 +14,7 @@ public class CustomerClient {
     public CustomerClient(@Value("${customer-service.base-url}") String baseUrl) {
         this.restClient = RestClient.create(baseUrl);
     }
-// test
+// testar på min bärbara att allt fungerar här också
     public boolean customerExists(Long customerId) {
         try {
             restClient.get().uri("/api/customer/{id}", customerId).retrieve().toBodilessEntity();
